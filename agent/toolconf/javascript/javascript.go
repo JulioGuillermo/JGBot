ef5120551ca905f83fb0f9042d7713c7d03b5e16
JulioGuillermo <julioguillermo0802@gmail.com>
2026-02-02 17:33:11 +0100

    adding js tool an skill

--- agent/toolconf/javascript/javascript.go
+++ agent/toolconf/javascript/javascript.go
@@ -3,7 +3,10 @@ package javascript
 import (
 	"JGBot/agent/tools"
 	"JGBot/ctxs"
+	"JGBot/files"
 	"JGBot/js/exec"
+	"JGBot/js/jsaddons/httpaddon"
+	"JGBot/js/jsaddons/virtualfilesaddon"
 	"JGBot/js/runners"
 	"context"
 	"fmt"
@@ -19,12 +22,22 @@ func (c *JavaScriptInitializerConf) Name() string {
 	return "javascript"
 }
 
-func (c *JavaScriptInitializerConf) ToolInitializer(ctx *ctxs.RespondCtx) tools.Tool {
+func (c *JavaScriptInitializerConf) ToolInitializer(rCtx *ctxs.RespondCtx) tools.Tool {
 	return &tools.ToolAutoArgs[JavaScriptArgs]{
 		ToolName:        c.Name(),
 		ToolDescription: "Executes sandboxed JavaScript code (ES2023). Ideal for complex math, data parsing, or logic.",
 		ToolFunc: func(ctx context.Context, args JavaScriptArgs) (string, error) {
-			output, err := runners.RunCode(args.Code, exec.FlagAsync(), exec.WithAwait())
+			sharedRoot, err := files.GetVirtualRoot(rCtx.SessionConf.Origin, "Shared")
+			if err != nil {
+				return "", err
+			}
+			output, err := runners.RunCode(
+				args.Code,
+				httpaddon.WithHttp(),
+				virtualfilesaddon.WithVirtualFile("VirtualFiles", sharedRoot),
+				exec.FlagAsync(),
+				exec.WithAwait(),
+			)
 			if err != nil {
 				return "", fmt.Errorf("ERROR: Fail to execute the JavaScript code: %s.", err.Error())
 			}
